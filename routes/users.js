c3689036f219947a9527998b3cd3f8f92e95c802
abiyaddisM <130771857+abiyaddisM@users.noreply.github.com>
2024-06-14 11:04:20 +0300

    new

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('hey bitchessss');
+  res.json({message:'Bitch'});
 });
 
 module.exports = router;
